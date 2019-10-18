a2749f2364989fa1092e23fc492888c12f8c8aac
snolly <snolly.gv@gmail.com>
2019-10-18 12:55:52 +0300

    add create client page

--- src/app/system/clients/create/create-client-by-user/create-client-by-user.component.ts
+++ src/app/system/clients/create/create-client-by-user/create-client-by-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-client-by-user',
@@ -8,6 +9,62 @@ import { Location } from '@angular/common';
 })
 export class CreateClientByUserComponent implements OnInit {
 
+  Form: FormGroup = new FormGroup({
+    'name': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30)
+    ]),
+    'country': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30)
+    ]),
+    'city': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30)
+    ]),
+    'street': new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30)
+    ]),
+    'house': new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999)
+    ]),
+    "store_id": new FormControl('',[
+      Validators.required
+    ])
+  });
+
+  get name() {
+    return this.Form.get('name');
+  }
+  get country() {
+    return this.Form.get('country');
+  }
+  get city() {
+    return this.Form.get('city');
+  }
+  get street() {
+    return this.Form.get('street');
+  }
+  get house() {
+    return this.Form.get('house');
+  }
+
+  SelectedStore = null;
+  ShowSelect = false;
+
+  LoalityProgramm = [
+    {id: 1, name: 'Первая акция'},
+    {id: 2, name: 'Вторая акция'},
+    {id: 3, name: 'Третья акция'}
+  ];
+
   constructor(private _location: Location) { }
 
   ngOnInit() {
@@ -15,6 +72,19 @@ export class CreateClientByUserComponent implements OnInit {
 
   GoBack() {
       this._location.back();
+  }
+
+  Save() {
+    console.log(`Save Form`);
+  }
+
+
+
+  OnSelected(item)
+  {
+    this.SelectedStore = item;
+    this.Form.controls.store_id.setValue(this.SelectedStore.id);
+    this.ShowSelect = false;
   }
 
 }
