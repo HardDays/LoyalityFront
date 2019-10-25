97d1143a84bd47ceef154a12c49193e0c975b950
kparamonov <prmnr2k@gmail.com>
2019-10-25 13:59:07 +0300

    microfix

--- src/app/system/promotions/promotions.routing.ts
+++ src/app/system/promotions/promotions.routing.ts
@@ -7,8 +7,8 @@ import { PromotionEditComponent } from './edit/edit.component';
 import { PromotionsAccessGuard } from './promotions.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'edit/2', pathMatch: 'full'},
-    // { path: '', redirectTo: 'list', pathMatch: 'full'},
+    // { path: '', redirectTo: 'edit/2', pathMatch: 'full'},
+    { path: '', redirectTo: 'list', pathMatch: 'full'},
     { path: 'list', component: PromotionsListComponent, canActivate:[PromotionsAccessGuard]},
     { path: 'edit/:id', component: PromotionEditComponent,canActivate:[PromotionsAccessGuard]}
 ];
