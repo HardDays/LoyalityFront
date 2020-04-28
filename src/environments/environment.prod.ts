1684b2fdf0d3a99ae071770218a51d20ab1ebf98
Ilfat Mindubaev <yagarantuz@gmail.com>
2020-04-28 20:30:27 +0300

    Change api url to prod version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fathomless-earth-40434.herokuapp.com/test/api/v1',
-  redirectUri: 'https://oauth.vk.com/blank.html',
+  apiUrl: 'https://fathomless-earth-40434.herokuapp.com/production',
   vkClientId: '7357954'
 };
