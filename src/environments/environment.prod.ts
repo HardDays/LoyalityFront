84a66ae5921d5e7d1d130f2f02b5cf59f3fbc6b6
Ilfat Mindubaev <i.mindubaev@portavita.ru>
2020-04-30 17:03:47 +0300

    Fix api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fathomless-earth-40434.herokuapp.com/production',
+  apiUrl: 'https://fathomless-earth-40434.herokuapp.com/production/api/v1',
   vkClientId: '7357954'
 };
