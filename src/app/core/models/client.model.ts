59b95984d16b5579cb1dab882d44e2492253e673
Kirill Paramonov <prmn.r2k@gmail.com>
2022-05-19 21:25:57 +0300

    final #1

--- src/app/core/models/client.model.ts
+++ src/app/core/models/client.model.ts
@@ -15,3 +15,22 @@ export class ClientModel {
     public loyalty_program?: any
   ) { }
 }
+
+export class ClientExtendedModel{
+  constructor(
+    public id?: number,
+    public phone?: string,
+    public first_name?: string,
+    public last_name?: string,
+    public second_name?: string,
+    public full_name?: string,
+    public gender?: string,
+    public birth_day?: string,
+    public loyalty_program_id?: number,
+    public card_number?: string,
+    public points?: number,
+    public recommendator_phone?: string,
+    public email?: string,
+    public loyalty_program?: any
+  ) { }
+}
--- src/app/system/bonus-management/bonus-overview/bonus-overview.component.ts
+++ src/app/system/bonus-management/bonus-overview/bonus-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BonusManagementService } from "../bonus-management.service"
-import { ClientModel } from 'src/app/core/models/client.model';
+import { ClientExtendedModel, ClientModel } from 'src/app/core/models/client.model';
 import { BonusStateService } from '../bonus-state.service';
 import { Router } from '@angular/router';
 
@@ -11,17 +11,39 @@ import { Router } from '@angular/router';
 })
 export class BonusOverviewComponent implements OnInit {
 
-  Clients: ClientModel[] = [];
-  _origClients: ClientModel[] = [];
+  Clients: ClientExtendedModel[] = [];
+  _origClients: ClientExtendedModel[] = [];
   userSearchData: string = "";
 
   constructor(private service: BonusManagementService, private stateService: BonusStateService, private router: Router) { }
 
   ngOnInit() {
     this.service.GetClients(
-      (data: ClientModel[]) => {
-        this.Clients = data;
-        this._origClients = data;
+      (data: ClientExtendedModel[]) => {
+        let arr = [];
+        data.forEach(client => {
+          let val = [];
+          if(client.first_name)
+          {
+            val.push(client.first_name);
+          }
+
+          if(client.last_name)
+          {
+            val.push(client.last_name);
+          }
+
+          if(client.second_name)
+          {
+            val.push(client.second_name);
+          }
+
+          client.full_name = val.join(" ");
+
+          arr.push(client);
+        });
+        this.Clients = arr;
+        this._origClients = arr;
       },
       (err) => {
         console.error(err)
@@ -38,16 +60,24 @@ export class BonusOverviewComponent implements OnInit {
     this.Clients = this.userSearchData
       ? this._origClients.filter(c => {
         let res = false;
-        if (c.first_name) {
+        if(c.full_name)
+        {
+          res = res || c.full_name.includes(this.userSearchData);
+        }
+        if (c.first_name) 
+        {
           res = res || c.first_name.includes(this.userSearchData);
         }
-        if (c.last_name) {
+        if (c.last_name) 
+        {
           res = res || c.last_name.includes(this.userSearchData);
         }
-        if (c.second_name) {
+        if (c.second_name) 
+        {
           res = res || c.second_name.includes(this.userSearchData);
         }
-        if (c.phone) {
+        if (c.phone) 
+        {
           res = res || c.phone.includes(this.userSearchData);
         }
         if(c.card_number)
